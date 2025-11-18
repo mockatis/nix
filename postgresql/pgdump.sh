8c1301e9a54cdd16fb469773aa11d9f970c5322a
mockatis <144959697+mockatis@users.noreply.github.com>
2025-11-18 09:36:57 +0300

    Update pgdump.sh

--- postgresql/pgdump.sh
+++ postgresql/pgdump.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 PATH=/etc:/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin
 
-PGPASSWORD="123123123"
+PGPASSWORD="PGPASSWORD"
 export PGPASSWORD
 pathB=/mnt/backup/dumps
 
 find $pathB \( -name "*-1[^5].*" -o -name "*-[023]?.*" \) -ctime +7 -delete
 
-for dbname in `echo "SELECT datname FROM pg_database;" | psql | tail -n +3 | head -n -2 | egrep -v 'template0|template1|postgres'`; do
+for dbname in `echo "SELECT datname FROM pg_database;" | psql | tail -n +3 | head -n -2 | egrep -v 'template0|template1|postgres|user1c'`; do
     pg_dump -h "x.x.x.x" -p "yyyyy" -U "postgres" $dbname | gzip > $pathB/$dbname-$(date "+%Y-%m-%d").sql.gz
 done;
 
